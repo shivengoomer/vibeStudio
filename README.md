4d9fabf603d4059a983ff37c545ded17874f1848
Shiven Goomer <shivengoomer@gmail.com>
2025-07-01 21:59:50 +0530

    html css js func

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# vibeStudio
+
--- index.js
+++ index.js
@@ -17,8 +17,6 @@ function executeCommand(command) {
   }
   
   let appName = '';
-  let techStack = '';
-  let folderName=''
 figlet.text('VibeStudio', {
     font: 'Standard',
     horizontalLayout: 'default',
@@ -41,37 +39,55 @@ figlet.text('VibeStudio', {
       appName = await input({ message: "What do you want me to make :" });
       if (!appName) console.log(chalk.red("Please provide a tech stack or app idea."));
   }
-  while (!techStack) {
-      techStack = await input({ message: "tech Stack? :" });
-      if (!techStack) console.log(chalk.red("Please provide a location."));
-  }
-      while (!folderName) {
-      folderName = await input({ message: "Path  :" });
-      if (!folderName) console.log(chalk.red("Please provide a location."));
-  }
-  let commands='echo hi'
-  const fullPrompt =`You are a senior software engineer with expertise in the ${techStack} and use only required folder or stack. Please provide a concise, step-by-step list of terminal commands to set up and run the ${appName} application, including any necessary file content modifications. Keep it strictly technical and executable
-  provide me only terminal commands in right order to be performed one by one making the app fully functional and also providing basic good appealing ui not basic but properly working application.
-  Make sure you give scripts and commands that are perfectly working!
-  dont include bash keyword at top or provide me each command.Give commands such that make sure everyfile is under the folder ${folderName}/${appName} example- 'mkdir ${folderName}/${appName}' then cd ${appName} then makes files.
-  you need to always first give commands- 'mkdir ${folderName}/${appName}' then cd ${folderName}/${appName}. then all the files will ne inside the app folder and echo the files in ${appName} example- touch ${folderName}/${appName}/requiredFieName(Stack Files)
-  just excute commands under ${executeCommand(commands)} where "commands" represents terminal commands one by one as string sepratedby \n `
-commands=await groqReturn(fullPrompt);
-  function splitCommandsByCaret(commands) {
+  let techStack="html css js"
+  let command='echo hi'
+  const fullPrompt=`Build a fully functional and modern-looking ${appName} application using HTML, CSS, and JavaScript only.
+
+Requirements:
+- Provide only terminal commands.
+- Every file must be created under the ${appName} folder.
+- Start with: mkdir ${appName} && cd ${appName}
+- All echo commands must follow this format: echo "content" > ${appName}/filename
+- Each command should be printed one by one, line by line (no comments or explanations).
+- Use touch to create files before echo.
+- use the same ${appName} user provide removing space.
+  }Include a good modern UI (not basic).
+- Application must be 100% working when opened in the browser.
+
+only commands no bash keyword`
+  
+const commands = await groqReturn(fullPrompt);
+function splitCommandsByCaret(commands) {
     return commands
     .split('\n')               // Split by caret symbol
     .map(cmd => cmd.trim())   // Trim whitespace from each command
     .filter(cmd => cmd);      // Remove empty strings
   }
+  function replaceFilePaths(cmd, appName) {
+    // Replace > script.js, > index.html, > style.css with > ${appName}/script.js etc.
+    const cleanAppName = appName.replace(/\s+/g, '');
+
+    // Replace > script.js, > index.html, > style.css with > ${appName}/filename
+    let newCmd = cmd.replace(/> ?(script\.js|index\.html|style\.css)/g, (match, p1) => {
+      return `> ${cleanAppName}/${p1}`;
+    });
+
+    // Replace touch script.js, touch index.html, touch style.css with touch ${appName}/filename
+    newCmd = newCmd.replace(/touch +(script\.js|index\.html|style\.css)/g, (match, p1) => {
+      return `touch ${cleanAppName}/${p1}`;
+    });
+
+    return newCmd;
+  }
+
   for (const cmd of splitCommandsByCaret(commands)) {
+    const fixedCmd = replaceFilePaths(cmd, appName);
     try {
-      const output = await executeCommand(cmd.replace(/^"|"$/g, ''));
-      console.log(chalk.green(`\n$ ${cmd}\n${output}`));
+      const output = await executeCommand(fixedCmd.replace(/^"|"$/g, ''));
     } catch (err) {
-      console.log(chalk.red(`\nError executing: ${cmd}\n${err}`));
+      console.log(chalk.red(`\nError executing: ${fixedCmd}\n${err}`));
       break;
     }
   }
-
 })();
 
